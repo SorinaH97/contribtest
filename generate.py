1b04b294cab8329339e109074bc655955f038b8d
SorinaH97 <62327542+SorinaH97@users.noreply.github.com>
2020-03-18 14:32:35 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,24 +1,25 @@
-# generate site from static pages, loosely inspired by Jekyll
-# run like this:
-#   ./generate.py test/source output
-# the generated `output` should be the same as `test/expected_output`
-
 import os
 import logging
 import jinja2
+import sys
+import json
 
 log = logging.getLogger(__name__)
 
-
+#checks all files in the folder_path and yields all .rst files
 def list_files(folder_path):
+    
     for name in os.listdir(folder_path):
         base, ext = os.path.splitext(name)
         if ext != '.rst':
             continue
         yield os.path.join(folder_path, name)
 
+#Reads the content of file_path and extract the data
+#Content starts where metadata ends (with '---')
 def read_file(file_path):
-    with open(file_path, 'rb') as f:
+    
+    with open(file_path, 'r') as f:
         raw_metadata = ""
         for line in f:
             if line.strip() == '---':
@@ -29,20 +30,30 @@ def read_file(file_path):
             content += line
     return json.loads(raw_metadata), content
 
+#Write the output in test folder
 def write_output(name, html):
-    # TODO should not use sys.argv here, it breaks encapsulation
-    with open(os.path.join(sys.argv[2], name+'.html')) as f:
+    with open(os.path.join('test', name+'.html'), 'w') as f:
         f.write(html)
 
-def generate_site(folder_path):
-    log.info("Generating site from %r", folder_path)
-    jinja_env = jinja2.Environment(loader=FileSystemLoader(folder_path + 'layout'))
-    for file_path in list_files(folder_path):
+def generate_site(input_folder):
+    # generates the site from the layout of the page by creating a jinga environment
+    log.info("Generating site from %r", input_folder)
+    jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(input_folder + '/layout'))
+
+    for file_path in list_files(input_folder):
+        #for every file in the input_folder, we extract the metadata and content
         metadata, content = read_file(file_path)
-        template_name = metadata['template']
+        
+        #extract and load the template
+        template_name = metadata['layout']
         template = jinja_env.get_template(template_name)
+        
+        #render the date using the template
         data = dict(metadata, content=content)
-        html = template(**data)
+        html = template.render(data)
+
+        #create the .html file
+        name = os.path.splitext(os.path.basename(file_path))[0]
         write_output(name, html)
         log.info("Writing %r with template %r", name, template_name)
 
@@ -52,5 +63,5 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig() 
     main()
